e854d205e59da3653b50cc08ff5f12be13ec279f
dushabella <izuu12@gmail.com>
2019-12-09 00:47:44 +0100

    ex15 definitely done - faster version added

--- Rosalind/ex15.py
+++ Rosalind/ex15.py
@@ -1,4 +1,4 @@
-from typing import Set, List
+from typing import List
 
 dna = str()
 with open("input.txt", "r") as file:
@@ -9,33 +9,17 @@ with open("input.txt", "r") as file:
             dna += line[:-1]
 print(dna)
 
-# set of prefixes ending at k-1
-# prefixes = [1, l] where l = <1, k-1>
-def find_prefixes(sequence: str, k:int) -> Set[str]:
-    prefixes = [sequence[:x] for x in range(k+1)]
-    prefixes = set(prefixes)
-    return prefixes
+def failure_array(sequence: str) -> List[int]:
+    n = len(sequence)
+    P = [0] * n
+    k = 0
+    for i in range(1, n):
+        while k > 0 and sequence[i] != sequence[k]:
+            k = P[k - 1]
+        if sequence[k] == sequence[i]:
+            k += 1
+        P[i] = k
+    return P
 
-# set of suffixes ending at k
-# suffixes = [j, k] where j = <2, k>
-def find_suffixes(sequence: str, k:int) -> Set[str]:
-    suffixes = [sequence[x+1:k+1] for x in range(k+2)]
-    suffixes = set(suffixes)
-    return suffixes
-
-#TODO: ask Piotrek how to run functions in a pretty way
-# - I mean how to run failure_array which is "main" function
-
-def failure_array(sequence: str, k:int) -> List[int]:
-    failure_array = list()
-    for i in range(k):
-        prefixes = find_prefixes(sequence, i)
-        suffixes = find_suffixes(sequence, i)
-        intersected = prefixes.intersection(suffixes)
-        longest = max(intersected, key=len)
-        len_of_longest = len(longest)
-        print(len_of_longest, end=" ")
-        failure_array.append(len_of_longest)
-    return failure_array
-
-failure_array(dna, len(dna))
+for element in failure_array(dna):
+    print(element, end=" ")
---
+++ Rosalind/ex15_slow_version.py
@@ -0,0 +1,41 @@
+from typing import Set, List
+
+dna = str()
+with open("input.txt", "r") as file:
+    for line in file:
+        if line.startswith(">"):
+            continue
+        else:
+            dna += line[:-1]
+print(dna)
+
+# set of prefixes ending at k-1
+# prefixes = [1, l] where l = <1, k-1>
+def find_prefixes(sequence: str, k:int) -> Set[str]:
+    prefixes = [sequence[:x] for x in range(k+1)]
+    prefixes = set(prefixes)
+    return prefixes
+
+# set of suffixes ending at k
+# suffixes = [j, k] where j = <2, k>
+def find_suffixes(sequence: str, k:int) -> Set[str]:
+    suffixes = [sequence[x+1:k+1] for x in range(k+2)]
+    suffixes = set(suffixes)
+    return suffixes
+
+#TODO: ask Piotrek how to run functions in a pretty way
+# - I mean how to run failure_array which is "main" function
+
+def failure_array(sequence: str, k:int) -> List[int]:
+    failure_array = list()
+    for i in range(k):
+        prefixes = find_prefixes(sequence, i)
+        suffixes = find_suffixes(sequence, i)
+        intersected = prefixes.intersection(suffixes)
+        longest = max(intersected, key=len)
+        len_of_longest = len(longest)
+        print(len_of_longest, end=" ")
+        failure_array.append(len_of_longest)
+    return failure_array
+
+failure_array(dna, len(dna))

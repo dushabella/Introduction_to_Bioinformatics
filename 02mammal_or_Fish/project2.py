d44c2014191fe98ca01b69567891040518e1afc4
dushabella <izuu12@gmail.com>
2020-01-03 23:50:32 +0100

    Mini Project #2: distance matrices ready but not checked :D linkage and dendrograme work Finding the best gene for distinguish still needed

--- 02mammal_or_Fish/project2.py
+++ 02mammal_or_Fish/project2.py
@@ -4,7 +4,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 from collections import defaultdict
 from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
 
 def read_file(path: str) -> Dict:
     ''' Reading genes from a file into a dictionary
@@ -22,6 +21,7 @@ def read_file(path: str) -> Dict:
                 values = line.split("\t")
                 genes[temp_gene_name].append(values)
     genes = dict(genes)
+
     return genes
 
 # X = [[i] for i in [2, 8, 0, 4, 1, 9, 9, 0]]
@@ -34,10 +34,6 @@ def read_file(path: str) -> Dict:
 # dn = dendrogram(Z)
 # print("average: ", Z)
 
-# alignments = pairwise2.align.globalms("ACCGT", "ACG", 1, -1, -2, 0) # Last one: -2? -1? 0? 1?
-# print(type(format_alignment(*alignments[0])))
-# print(alignments[0][2])
-
 def distance_matrices(genes: Dict) -> Dict: #Dict[List[List[int]]]
     gene_matrices = defaultdict(list)
     for key in genes:
@@ -45,35 +41,66 @@ def distance_matrices(genes: Dict) -> Dict: #Dict[List[List[int]]]
         mtrx = []
         for species in genes[key]:
             species_gene1 = species[-1]
-            # print("..............")
-            # print(species_gene1)
-            # print("lolll")
             row = []
             for species in genes[key]:
                 i+=1
                 species_gene2 = species[-1]
                 # print(species_gene2)
                 alignment = pairwise2.align.globalms(species_gene1, species_gene2, 1, -1, -2, -2)
-                row.append(alignment[0][2])
-                # print(i)
+                # print(alignment[0][0])
+                # print(alignment[0][1])
+                distance = count_distance(alignment[0][0], alignment[0][1])
+                row.append(distance)
             mtrx.append(row)
             gene_matrices[key].append(row)
-        print(key, mtrx)
+        # print(key, mtrx)
         # gene_matrices[key].append(mtrx)
 
     gene_matrices = dict(gene_matrices)
     return gene_matrices
 
+
+def count_distance(str1: str, str2: str) ->float:
+    ''' Returns the distance between two strings,
+    corrected according to Juke-Cantor model.
+    http://readiab.org/book/0.1.3/2/4 '''
+
+    res = 0
+
+    # Hamming distance
+    i = 0
+    if len(str1) != len(str2):
+        raise Exception('Compared strings must be the same length!')
+    for position in str1:
+        if position != str2[i]:
+            res += 1
+        i += 1
+
+    # Jukes-Cantor correction
+    res = res/len(str1)
+
+    # Jukes-Cantor (JC69)
+    res = -3/4 * np.log(1 - 4/3 * res)
+    return res
+
+
 def show_dendrograme(distance_matrices: dict) ->None:
-    print(distance_matrices)
     for key in distance_matrices:
-        print(distance_matrices[key])
+        # print(distance_matrices[key])
         upgma = linkage(distance_matrices[key], 'average')
-        fig = plt.figure(figsize=(25, 10))
+        fig = plt.figure(figsize=(12, 4))
         dn = dendrogram(upgma)
+        plt.title(key)
+        plt.show()
+
 
 genes = read_file('genes.txt')
 
 distances = distance_matrices(genes)
+for key in distances:
+    print(key)
+    print(distances[key])
+    print("-----------")
 
 show_dendrograme(distances)
+

a99212113407574836b58e62c4707c13289dd468
dushabella <izuu12@gmail.com>
2020-01-19 02:02:13 +0100

    Mini Project #3: recursion assembly function

--- 03_genome_reconstruction/project3.py
+++ 03_genome_reconstruction/project3.py
@@ -1,15 +1,17 @@
 """
 Mini project #3:
 ---------------
+A project is focused on finding the minimum length k of k-mers for which it is possible to obtain unique cyclic genome.
+It bases on constructing de Bruijn graph with k-mers on a nodes, which indeed is a solution of Eulerian path problem.
 
-The project is focused on finding the minimum length k of k-mers for which it is possible to obtain unique cyclic genome.
-Implementation of a whole method for constructing Hamiltionian cycle which recovers the genome wasn't necessary.
-
-Good source of knowledge: https://www.youtube.com/watch?v=0JlUy_l-RTk&t=2s
+Good source of knowledge:
+http://www.bioinformatika.matf.bg.ac.rs/predavanja/literatura/Phillip%20Compeau,%20Pavel%20Pevzner%20-%20Bioinformatics%20Algorithms_%20An%20Active%20Learning%20Approach.%201-Active%20Learning%20Publ.%20(2015).pdf
+[pages 121-138]
 """
 
-from typing import Dict, List, Tuple
-from collections import defaultdict
+from typing import Dict, List, Set, Tuple
+from sys import getrecursionlimit, setrecursionlimit
+
 
 def read_file(path: str) -> Dict:
     """ Reading genes from a file into a dictionary
@@ -33,11 +35,9 @@ def composition(genomes: str, k_len: int) -> List[str]:
     """
     Takes a circular genes and divide it for all possible k-mers (fragments) of a size of "k-mer" parameter
     and returns a list of k-mers that "wrap around" the end.
-
     For example:
         in: composition(“ATACGGTC”, 3)
         out: [“ATA”, “TAC”, “ACG”, “CGG”, “GGT”, “GTC”, “TCA”, “CAT”]
-
     :param gene: gene for reconstruction
     :param k_len: size of each divided kmer (fragment)
     :return fragments: list of kmers """
@@ -56,98 +56,106 @@ def composition(genomes: str, k_len: int) -> List[str]:
     return(fragments)
 
 
-def suffix_prefix(kmers: List[str]) ->Tuple[Dict, int]:
-    """
-    Returns a prefix and a suffix of each k-mer from the list.
-
-    :param kmers: A list of error-free DNA k-mers taken from the strand of circular chromosome
-    :return unique: 1 - assembled genome will be unique; 0 - ambiguous
-    """
-
-    # Quick result: at the end, check if the len(result) is the same as len(kmers)\
-    # if yes - the circular genome won't be ambiguous (will be unique)
-
-    kmer_len = len(kmers)
-    unique = 0
-
-    result = defaultdict(list)
-    for kmer in kmers:
-        prefix = kmer[:-1]
-        suffix = kmer[1:]
-        result[suffix].append(prefix)
-        # print("suffix:", suffix, "prefiks: ", result[suffix])
-        # print("__________")
-    result = dict(result)
-    result_len = len(result)
-    # print('\033[93m' + "dict[suffix] = prefix:" + '\033[0m', result)
-    if kmer_len == result_len:
-        print('\033[94m' + "The number of kmers and distinctive prefixes are the same, so circular gene will be unique!" + '\033[0m')
-        unique = 1
-
-    return(result, unique)
-
-
-def is_unique(pref_suf: Dict) -> int:
-    """
-    Checks if the circular genome assembled with these mers will be unique.
-
-    :param pref_suf: A dictionary that contains suffixes of kmers and prefixes which correspond them: pref_suf[suffix] = prefixes
-    :return unique: 1 - assembled genome will be unique; 0 - ambiguous
-    """
-
-    unique = 1 # assembled genome will be unique
-
-    # check if in lists of prefixes that correspond to each suffix are redundances \
-    # if yes - leave only one of each prefix
-    for key in pref_suf:
-        prefixes = pref_suf[key]
-        # print("Prefixes of the suffix", key, "are", prefixes)
-
-        for prefix in prefixes:
-            number = prefixes.count(prefix)
-            if number > 1:
-                prefixes.remove(prefix)
-        pref_suf[key] = prefixes
-
-    # check how many prefixes is in each list
-    # if each list of prefixes still consist of more than one element - assembled genome will be ambiguous for sure
-    for key in pref_suf:
-        prefixes = pref_suf[key]
-        if len(prefixes) > 1:
-            unique = 0
-            print('\033[91m' + "Suffix has more than one distinguishable prefix, so circular gene will be ambiguous!" + '\033[0m')
-            break
-
-    if unique == 1:
-        print('\033[94m' + "Circular gene will be unique!" + '\033[0m')
-
-    return(unique)
-
 def simple_reconstruction(kmers: List[str]) ->str: #->List[str]:
     """
-    Reconstruction of a circular string from a k-mers using de Bruijn graph.
-    http://rosalind.info/problems/grep/
-
+    Reconstruction of a circular genome of k-mers
     For example:
-        circular string assembled from the cycle "AC" -> "CT" -> "TA" -> "AC" is simply (ACT)
-    :param kmers: A list of error-free DNA (k+1) mers taken from the strand of circular chromosome
-    :return result: one of the circular strings assembled by complete cycles in the Bruijn graph. """
+        circular string assembled from the cycle "AC" -> "CT" -> "TA" is simply (ACT)
+    :param kmers: A list of mers
+    :return result: circular string """
 
     result = ""
     i = 0
     for kmer in kmers:
-        if i == 0:
-            result = kmer
-        elif i < len(kmers) - len(kmer) + 1:
-            result += kmer[-1]
+        result += kmer[-1]
         i += 1
 
-    # print(result)
     return(result)
 
 
+def assembly(kmers: List[str], assembled: List[str], found_genomes: Set[str]) -> Set[str]:
+    """
+    Recursive function for genome assembly using k-mers as edges of de Bruijn graph and prefixes/suffixes as nodes.
+    It solves Eulerian path problem in effect.
+
+    :param kmers: list of k-mers of genome sequence
+    :param assembled: [RECURSION PARAM] put empty list here
+    :param found_genomes: [RECURSION PARAM] put empty set here
+
+    :return found_genomes: genomes that might be constructed from given fragments. """
+
+    if len(kmers) == 0:  # end
+        new_genome = simple_reconstruction(assembled)
+        found_genomes.add(new_genome)
+
+        return found_genomes
+
+    elif len(assembled) == 0:  # begin
+        assembled.append(kmers[0])
+        found_genomes = assembly(kmers[1:], assembled, found_genomes)
+    else:
+        possible_to_join_id = [i for i, mer in enumerate(kmers) if mer[:-1] == assembled[-1][1:]] # if suffix == prefix
+        for index in possible_to_join_id:
+            assembled.append(kmers[index])
+            found_genomes = assembly(kmers[:index] + kmers[index+1:], assembled, found_genomes)
+            del assembled[-1]
+
+    return found_genomes
+
+
+def is_same_as_original(original: str, assembled: str) -> bool:
+    """
+    Checks if achieved genome assembly is same as original genome.
+
+    :param original:
+    :param created:
+
+    :return the_same: True - identical or identical but shifted """
+    concatenated = assembled + assembled
+    the_same = original in concatenated
+    return the_same
+
+
+def remove_redundant(found_genomes: Set[str]) -> List[str]:
+    """
+    Removes redundant found_genomes genomes (like 'CTGACATA' and 'CATACTGA')
+
+    :param found_genomes:
+
+    :return result: list with unique found_genomes genome """
+    found_genomes = list(found_genomes)
+    i = 0
+    while i < len(found_genomes):
+        if any([is_same_as_original(found_genomes[i], found_genomes[k]) for k in range(len(found_genomes)) if i != k]):
+            del found_genomes[i]
+        else:
+            i += 1
+    return found_genomes
+
+
+def find_min_k(genome: str, k: int=30) -> str:
+    """
+    Returns minimum k found for building k-mers that will achieve only one unique and correct genome from genome assembly
+    operation.
+    Starts from high value of k because the recursive assembly function is getting slow for low k.
+
+    :param genome: input genom that is first decomposed for k-mers
+    :param k: k-mer length from which the searching algorithm starts
+    :return min_k: minimum found k """
+
+    while k > 5:
+        comp = composition(genome, k)
+        created = assembly(comp, list(), set())
+        created = remove_redundant(created)
+        if len(created) > 1:
+            return k+1
+        last = created[0]
+        k -= 1
+    return k+1
+
+
 def main():
-    result = dict()
+    setrecursionlimit(getrecursionlimit()*2)
 
     genomes = read_file("genomes.txt")
     for key in genomes:
@@ -155,25 +163,8 @@ def main():
         print("Name:", key)
         genome = genomes[key]
         print("genome:", genome)
-
-        result[key] = None
-
-        for i in range(len(genome)):
-            k = i+1
-            kmers = composition(genome, k)
-            simple_reconstruction(kmers)
-            pref_suf, unique = suffix_prefix(kmers)
-            if unique == 1:
-                result[key] = k
-                # reconstruction for checking
-                break
-            else:
-                unique2 = is_unique(pref_suf)
-                if unique2 == 1:
-                    result[key] = k
-                    break
-
-    print(result)
+        min_k = find_min_k(genome)
+        print("min k: ", min_k)
 
 
 if __name__ == "__main__":
